fa3320eff2f8059b90e98e387266eafec3b629cf
VikudouSennin <cao.d.vi@gmail.com>
2018-12-25 21:46:11 +0700

    Revert "Update comp.h"
    This reverts commit 1002177386291f1871f675f132642110273ee51d.

--- comp.h
+++ comp.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "iostream"
 #include "vector"
+//#include <cstdint>
+
 
 using namespace std;
 
@@ -9,25 +11,37 @@ struct ipANDmask
 	vector<char> block;
 };
 
-struct LIST
-{
-	int size;
-	vector<COMP> comp;
-};
 
 class COMP
 {
 private:
 	int  name;
 	int size;
-	vector<int32_t> arr;
+	vector<ipANDmask> arr;
 public:
-	void setSize(int size)
+	void getSize(int size)
 	{
 		this->size = size;
 	}
-	void setData(int32_t data)
+
+	void getData(ipANDmask data)
 	{
 		this->arr.push_back(data);
 	}
+
+	bool isConnect(COMP com)
+	{
+		for (int i = 0; i < this->size; ++i)
+			for (int j = 0; j < com.size; ++j)
+				if (this->arr[i] == com.arr[j])
+					return true;
+		return false;
+	}
 };
+
+struct LIST
+{
+	int size;
+	vector<COMP> comp;
+};
+

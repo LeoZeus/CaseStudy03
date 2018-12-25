1002177386291f1871f675f132642110273ee51d
LeoZeus <35501477+LeoZeus@users.noreply.github.com>
2018-12-25 21:28:56 +0700

    Update comp.h

--- comp.h
+++ comp.h
@@ -1,8 +1,6 @@
 #pragma once
 #include "iostream"
 #include "vector"
-//#include <cstdint>
-
 
 using namespace std;
 
@@ -11,37 +9,25 @@ struct ipANDmask
 	vector<char> block;
 };
 
-
-class COMP
-{
-private:
-	int  name;
-	int size;
-	vector<ipANDmask> arr;
-public:
-	void getSize(int size)
-	{
-		this->size = size;
-	}
-
-	void getData(ipANDmask data)
-	{
-		this->arr.push_back(data);
-	}
-
-	bool isConnect(COMP com)
-	{
-		for (int i = 0; i < this->size; ++i)
-			for (int j = 0; j < com.size; ++j)
-				if (this->arr[i] == com.arr[j])
-					return true;
-		return false;
-	}
-};
-
 struct LIST
 {
 	int size;
 	vector<COMP> comp;
 };
 
+class COMP
+{
+private:
+	int  name;
+	int size;
+	vector<int32_t> arr;
+public:
+	void setSize(int size)
+	{
+		this->size = size;
+	}
+	void setData(int32_t data)
+	{
+		this->arr.push_back(data);
+	}
+};
